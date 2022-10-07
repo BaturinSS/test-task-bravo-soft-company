58873457d8c0fe84f5450fd53432e4da2ebd28de
Сергей Батурин <toko-nn@yandex.ru>
2022-10-07 14:57:57 +0300

    hotfix button submit

--- src/db/manuals.js
+++ src/db/manuals.js
@@ -1,47 +1,47 @@
 export const manuals = [
   {
     id: 1,
-    designer: "Иванов",
+    designer: "Иванов И. И.",
     manual: 'ГОСТ 123',
   },
   {
     id: 2,
-    designer: "Петров",
+    designer: "Петров П. П.",
     manual: 'ГОСТ 123',
   },
   {
     id: 3,
-    designer: "Сидоров",
+    designer: "Сидоров С. С.",
     manual: 'ГОСТ 123',
   },
   {
     id: 4,
-    designer: "Сидоров",
+    designer: "Сидоров С. С.",
     manual: 'ГОСТ 123456',
   },
   {
     id: 5,
-    designer: "Петров",
+    designer: "Петров П. П.",
     manual: 'ГОСТ 123456',
   },
   {
     id: 6,
-    designer: "Иванов",
+    designer: "Иванов И. И.",
     manual: 'ГОСТ',
   },
   {
-    id: 4,
-    designer: "Сидоров",
+    id: 7,
+    designer: "Сидоров С. С.",
     manual: 'ГОСТ',
   },
   {
-    id: 5,
-    designer: "Петров",
+    id: 8,
+    designer: "Петров П. П.",
     manual: 'ГОСТ 456',
   },
   {
-    id: 6,
-    designer: "Иванов",
+    id: 9,
+    designer: "Иванов И. И.",
     manual: 'ГОСТ 789',
   },
 ]
